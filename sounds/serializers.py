b31be118af2e4bd9f810fb0e72b01894b11a3c4e
yu-rkt <y.watanabe.rkt@gmail.com>
2020-02-14 19:35:24 +0900

    ユーザーモデルの追加

---
+++ sounds/serializers.py
@@ -0,0 +1,13 @@
+# from rest_framework import serializers
+# from .models import Content
+# from accounts.models import User
+
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ('')
+
+# class ContentSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Content
+#         fields = ('')
